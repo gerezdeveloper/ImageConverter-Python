42ba3206f7d0938e1007e792042df2fbc8d8f046
Andrew Gerez <119694726+gerezdeveloper@users.noreply.github.com>
2023-01-12 11:59:46 -0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -2,9 +2,13 @@ from PIL import Image    # Importa a biblioteca Pillow e o módulo para a conver
 import os        # Importa a função que permite o run do script.
 
 list_images = os.listdir("images")       # Variável para armazenar as imagens.
+                        #Nome da folder 
+                        #onde estão as imgs.
 
 for arquivo in list_images:        # Função para selecionar da primeira imagem até a última.
     imagem = Image.open(f"images/{arquivo}")     # Função para abrir cada imagem da pasta.
 
     imagem.save(f'svg/{arquivo.replace("png", "webp")}')      # Função para dar um replace junto com o save da imagem refatorada.
+             #Nome da pasta           #Formato #Formato
+             #onde será salvo.        #inicial #convertido
    
